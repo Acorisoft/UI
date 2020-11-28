91fd64c60e05c6d86b57590c75c6d05aa42d8181
Acoris <731984870@qq.com>
2020-11-28 23:03:45 +0800

    更新了ToolbarPanel

--- src/Acorisoft.UI/Properties/AssemblyInfo.cs
+++ src/Acorisoft.UI/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Windows;
 using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None , ResourceDictionaryLocation.SourceAssembly)]
-[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI","Acorisoft.UI.Buttons")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI" , "Acorisoft.UI.Buttons")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI" , "Acorisoft.UI.Panels")]
