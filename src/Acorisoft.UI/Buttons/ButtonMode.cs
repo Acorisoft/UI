7e001067ff24d42f05490c0fecd12c3dacffe7ca
Acoris <731984870@qq.com>
2020-11-28 12:27:24 +0800

    保存更改

---
+++ src/Acorisoft.UI/Buttons/ButtonMode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acorisoft.UI.Buttons
+{
+    public enum ButtonMode
+    {
+        Solid,
+        Outline,
+        CTA,
+        Opacity,
+        Transparent,
+        Link
+    }
+}
--- src/Acorisoft.UI/Properties/AssemblyInfo.cs
+++ src/Acorisoft.UI/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Windows;
 using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None , ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI","Acorisoft.UI.Buttons")]
