6536832812ddc12e7ea4d2fd4f224c253475dcda
Acoris <731984870@qq.com>
2020-11-30 17:14:02 +0800

    暂存更改

---
+++ src/Acorisoft.UI.Share/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None , ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI" , "Acorisoft.UI.Buttons")]
+[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI" , "Acorisoft.UI.Panels")]
---
+++ src/Acorisoft.UI.Share/Utils/BooleanBoxes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acorisoft
+{
+	public static class BooleanBoxes
+	{
+		public static object TrueBox = true;
+
+		public static object FalseBox = false;
+
+		public static object Box(bool value)
+		{
+			if (value)
+			{
+				return TrueBox;
+			}
+			return FalseBox;
+		}
+	}
+}
