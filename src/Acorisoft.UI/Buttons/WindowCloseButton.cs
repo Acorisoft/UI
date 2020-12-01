7d2889341fd643ad357b646533560775aeafd807
Acoris <731984870@qq.com>
2020-12-01 11:14:29 +0800

    update readme and add some controls

---
+++ src/Acorisoft.UI/Buttons/WindowCloseButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Acorisoft.UI.Buttons
+{
+    public class WindowCloseButton : ModeButton
+    {
+        static WindowCloseButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WindowCloseButton) , new FrameworkPropertyMetadata(typeof(WindowCloseButton)));
+        }
+    }
+}
