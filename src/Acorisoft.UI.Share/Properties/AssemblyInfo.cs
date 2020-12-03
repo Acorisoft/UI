79a7982fb0600abb53b45183d39e19cf4619f789
Acoris <731984870@qq.com>
2020-12-03 12:01:23 +0800

    移除无用的项目

--- src/Acorisoft.UI.Share/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Windows;
-using System.Windows.Markup;
-
-[assembly: ThemeInfo(ResourceDictionaryLocation.None , ResourceDictionaryLocation.SourceAssembly)]
-[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI" , "Acorisoft.UI.Buttons")]
-[assembly: XmlnsDefinition("https://github.com/Acorisoft/UI" , "Acorisoft.UI.Panels")]
---
+++ src/Acorisoft.UI/Controls/TransparentComboBox.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Acorisoft.UI.Controls
+{
+    /// <summary>
+    /// 透明组合框
+    /// </summary>
+    public class TransparentComboBox : ComboBox
+    {
+        static TransparentComboBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TransparentComboBox) , 
+                new FrameworkPropertyMetadata(typeof(TransparentComboBox)));
+        }
+
+
+        public Brush PopupForeground
+        {
+            get => (Brush)GetValue(PopupForegroundProperty);
+            set => SetValue(PopupForegroundProperty , value);
+        }
+
+        public static readonly DependencyProperty PopupForegroundProperty = DependencyProperty.Register(
+            "PopupForeground",
+            typeof(Brush),
+            typeof(TransparentComboBox), 
+            new PropertyMetadata(null));
+
+    }
+}
